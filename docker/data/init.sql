f2c7636486521e346d3901a06bb19b1ee4f3747f
Jongkeun Choi <jongkeun.ch@gmail.com>
2020-07-04 19:56:41 +0900

    nginx - express API server 연결

--- docker/data/init.sql
+++ docker/data/init.sql
@@ -1,5 +1,3 @@
 use board;
-CREATE TABLE IF NOT EXISTS `testggyu` ( 
-  `id` int(11) DEFAULT NULL, 
-  `resultTest` varchar(30)
-);
+
+CREATE TABLE IF NOT EXISTS `board`.`articles` ( `id` INT NOT NULL AUTO_INCREMENT COMMENT '게시물 id' , `board_id` INT NOT NULL COMMENT '게시물이 있는 게시판의 id' , `title` VARCHAR(512) NOT NULL COMMENT '제목' , `content` MEDIUMTEXT NULL COMMENT '내용' , PRIMARY KEY (`id`)) ENGINE = InnoDB COMMENT = '게시글들이 저장되는 곳입니다.';
