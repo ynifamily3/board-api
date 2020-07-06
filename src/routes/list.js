81bbabea9fe4e1e6ff6b9c663ce8adc0ea315017
Jongkeun Choi <jongkeun.ch@gmail.com>
2020-07-06 17:01:57 +0900

    프로젝트 구조 수정

--- src/routes/list.js
+++ src/routes/list.js
@@ -1,9 +1,17 @@
 var express = require("express");
+const mysql = require("mysql2");
 var router = express.Router();
+
+const connection = mysql.createConnection({
+  host: "db",
+  user: "miel",
+  password: "miel",
+  database: "board",
+});
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "list api 제공" });
+  res.render("index", { title: "list api 입니다." });
 });
 
 module.exports = router;
