cf607120a9ee4ac25d1e54ea13226c7524911744
Jongkeun Choi <jongkeun.ch@gmail.com>
2020-07-06 16:02:08 +0900

    소스코드 원복

--- src/app.js
+++ src/app.js
@@ -38,18 +38,4 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("listen 3000 port");
-});
-
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  res.status(err.status || 500);
-  res.render("error");
-});
+module.exports = app; // app.js 파일의 맨 아래 코드
